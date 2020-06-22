c17ac9c89da8167889e8f8d40e20fd555a6d7c2a
nthnmillar <nthnmillar@gmail.com>
2020-06-22 15:52:10 +1000

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import 'bootstrap'; 
+/* import logo from './logo.svg';
+import './App.css'; */
 
-function App() {
+class App extends React.Component {
+  /*
+  constructor(props){
+    this.state = {
+      input:""
+    };
+  }
+  */
+
+  render(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+      <div className="container-fluid">        
+        <h1 className="text-center">Markdown Editor</h1>
+          <div className="row">
+            <div className="col-sm-6">
+              <h2>Markdown</h2>
+              <textarea id="editor"/> 
+            </div>
+            <div className="col-sm-6">
+              <h2>Preview</h2>
+              <div id="preview"/>
+            </div>
+          </div>             
+      </div>
+    );
+  }
+};
 
 export default App;
