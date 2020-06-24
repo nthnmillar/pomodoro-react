1c89a01b334dcbfd9c092a629f0cc410f35e9baa
nthnmillar <nthnmillar@gmail.com>
2020-06-24 16:50:57 +1000

    Seperated sound functions into their own file

--- src/App.js
+++ src/App.js
@@ -1,22 +1,6 @@
 import React from 'react';
-import D2 from './audio/909-HiHatClosed-D2.wav';
-import 'bootstrap'; 
-
-function DrumSound(noise){
-    console.log("audio", noise);
-    let audio = new Audio(noise);
-    audio.play();
-}
-
-function SoundPlay(name){ 
-    console.log("SoundPlay","soundName", name);
-    switch(name){
-      case "Q":
-        DrumSound(D2);
-      default:
-        return 
-      }
-}
+import 'bootstrap';
+import SoundPlay from "./SoundPlay";
     
 function DrumButton(props){
     console.log("DrumButton", props.btnName);
@@ -52,63 +36,3 @@ function App(){
 }
 
 export default App;
-
-// import React, {useState} from 'react';
-/*import AudioPlayer from "./AudioPlayer"; */
-
-/*
-const [drumSound,setSound] = useState(0);
-const [btnName,setBtn] = useState(0);
-*/
- 
-
-//   
-// <SoundPlay drumSound={props.btnName}/>
-
- /*
-  const audioPlayer = AudioPlayer();
-  useEffect(() => {
-    audioPlayer.setInstrument("acoustic_grand_piano");
-  }, []);
-
-  const handleClick = () => {
-    audioPlayer.playNote("C4");
-  };
-  */
-
-/*
-
-          <div className="row">           
-            <div className="drum-pad col" id="">
-              <p className="text-center">Q</p>
-            </div>
-            <div className="drum-pad col" id="">
-              <p className="text-center">W</p>
-            </div>
-            <div className="drum-pad col" id="">
-              <p className="text-center">E</p>
-            </div>    
-          </div>
-          <div className="row">
-            <div className="drum-pad col" id="">
-              <p className="text-center">A</p>
-            </div>
-            <div className="drum-pad col" id="">
-              <p className="text-center">S</p>
-            </div>
-            <div className="drum-pad col" id="">
-              <p className="text-center">D</p>
-            </div> 
-          </div>
-          <div className="row">
-            <div className="drum-pad col" id="">
-              <p className="text-center">Z</p>
-            </div>
-            <div className="drum-pad col" id="">
-              <p className="text-center">X</p>
-            </div>
-            <div className="drum-pad col" id="">
-              <p className="text-center">C</p>
-            </div> 
-          </div>       
-          */
---
+++ src/SoundPlay.js
@@ -0,0 +1,52 @@
+import drum1 from './audio/909-HiHatClosed-D2.wav';
+import drum2 from './audio/909-HiHatOpen-D0.wav';
+import drum3 from './audio/909-HiTom-0D0.wav';
+import drum4 from './audio/909-HiTom-7D0.wav';
+import drum5 from './audio/909-HiTom-AD0.wav';
+import drum6 from './audio/909-Kick-T0A0A7.wav';
+import drum7 from './audio/909-Kick-T3A0D0.wav';
+import drum8 from './audio/909-Kick-T7A0D0.wav';
+import drum9 from './audio/909-LoTom-0D0.wav';
+
+function DrumSound(noise){
+    console.log("audio", noise);
+    let audio = new Audio(noise);
+    audio.play();
+}
+
+function SoundPlay(name){ 
+    console.log("SoundPlay","soundName", name);
+    switch(name){
+      case "Q":
+        DrumSound(drum1);
+        break;
+      case "W":
+        DrumSound(drum2);
+        break;
+      case "E":
+        DrumSound(drum3);
+        break;
+      case "A":
+        DrumSound(drum4);
+        break;
+      case "S":
+        DrumSound(drum5);
+        break;
+      case "D":
+        DrumSound(drum6);
+        break;
+      case "Z":
+        DrumSound(drum7);
+        break;
+      case "X":
+        DrumSound(drum8);
+        break;
+      case "C":
+        DrumSound(drum9);
+        break;                                     
+      default:
+        return 
+      }
+}
+
+export default SoundPlay;
