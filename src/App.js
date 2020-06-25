daf92bae13772e2ba893211aa2b8364d8984576e
nthnmillar <nthnmillar@gmail.com>
2020-06-25 13:31:08 +1000

    Refactored code to meet conditions of FCC test

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
+import drum1 from './audio/909-HiHatClosed-D2.wav';
+import drum2 from './audio/909-HiHatOpen-D0.wav';
+import drum3 from './audio/909-HiTom-0D0.wav';
+import drum4 from './audio/909-HiTom-7D0.wav';
+import drum5 from './audio/909-HiTom-AD0.wav';
+import drum6 from './audio/909-Kick-T0A0A7.wav';
+import drum7 from './audio/909-Kick-T3A0D0.wav';
+import drum8 from './audio/909-Kick-T7A0D0.wav';
+import drum9 from './audio/909-LoTom-0D0.wav';
 import React, {useState, useEffect } from 'react';
-import SoundPlay from './SoundPlay';
 import 'bootstrap';
 
 function useKey(key){
@@ -22,15 +30,26 @@ function useKey(key){
     }
   },[key])
 
-  SoundPlay(key);
+  console.log("pressed key", key)
+  console.log("press", press)
+  
+  if (document.readyState === "complete"){
+    playSound(key);  
+  }
+
   return press
+}
+
+function playSound(key){
+  document.getElementById(key).play();
 }
 
 function DrumButton(props){
     console.log("DrumButton", props.btnName);
     return (
-      <div className="drum-pad col" onClick={() => {SoundPlay(props.btnName)}}>
-        <p className="text-center">{props.btnName}</p>
+      <div className="drum-pad col" id={"drum-pad-" + props.btnName} onClick={() => {playSound(props.btnName)}} >
+        {props.btnName}       
+        <audio className="clip" id={props.btnName} src = {props.srcName}></audio>
         {useKey(props.btnName)}
       </div>
     )
@@ -42,19 +61,19 @@ function App(){
         <div id="display">
         </div>                 
           <div className="row" id="row-1">  
-            <DrumButton btnName="Q"/>
-            <DrumButton btnName="W"/>
-            <DrumButton btnName="E"/>                                  
+            <DrumButton btnName="Q" srcName={drum1}/>
+            <DrumButton btnName="W" srcName={drum2}/>
+            <DrumButton btnName="E" srcName={drum3}/>                                  
           </div>
           <div className="row">  
-            <DrumButton btnName="A"/>
-            <DrumButton btnName="S"/>
-            <DrumButton btnName="D"/>                                  
+            <DrumButton btnName="A" srcName={drum4}/>
+            <DrumButton btnName="S" srcName={drum5}/>
+            <DrumButton btnName="D" srcName={drum6}/>                                  
           </div>
           <div className="row">  
-            <DrumButton btnName="Z"/>
-            <DrumButton btnName="X"/>
-            <DrumButton btnName="C"/>                                  
+            <DrumButton btnName="Z" srcName={drum7}/>
+            <DrumButton btnName="X" srcName={drum8}/>
+            <DrumButton btnName="C" srcName={drum9}/>                                  
           </div>     
       </div>     
     )
