fe8b369b43d73fc399fbb6432ecdf632dff480c6
nthnmillar <nthnmillar@gmail.com>
2020-07-06 11:06:36 +1000

    Laid out base jsx/html

--- src/App.js
+++ src/App.js
@@ -1,5 +1,84 @@
 import React from 'react';
 import 'bootstrap';
+
+
+const BreakLength = () => {
+  return (
+    <div className = "choiceDiv text-center">
+      <h2 id ="break-label">BREAK LENGTH</h2>
+      <div className = "selectDiv">
+        <button onClick="" id ="break-decrement">-</button>
+        <p id="break-length"/>
+        <button onClick="" id ="break-increment">+</button>
+      </div>    
+    </div> 
+  )
+}
+
+
+const SessionLength = () => {
+  return (
+    <div className = "choiceDiv text-center">
+      <h2 id ="session-label">SESSION LENGTH</h2>
+      <div className = "selectDiv">
+        <button onClick="" id ="session-decrement">-</button>
+        <p id="break-length"/>
+        <button onClick="" id ="session-increment">+</button>
+      </div>    
+    </div> 
+  )
+}
+
+const TimeLabel = () => {
+  return (
+    <>
+      <span id="timer-label"></span>      
+    </>
+  )
+}
+
+const Time = () => {
+  return (
+    <>
+       <span id="time-left"></span> 
+    </>
+    )
+}
+
+const PieButton = () => {
+  return (
+    <>
+      <button id="start_stop" onClick="">
+        <div className="pie degree"/>       
+      </button>  
+    </>
+    )
+}
+
+const App = () =>{ 
+  return (
+      <div className= "container">   
+        <h1 className="text-center">Pomodoro Clock</h1>
+          <div className= "text-center" id="options">
+            <BreakLength/>
+            <SessionLength/>
+          </div>
+          <div className= "text-center">
+            <TimeLabel/>
+            <br/>
+            <Time/>
+          </div> 
+          <PieButton/>
+      </div>
+    )
+}
+
+export default App;
+
+
+/*
+import React from 'react';
+import 'bootstrap';
 import { Provider, connect } from 'react-redux';
 import store from "./store";
 import {displayEntryAction, displaySumAction} from './actions/displayActions';
@@ -81,3 +160,5 @@ const App = () =>{
 }
 
 export default App;
+
+*/
